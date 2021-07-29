9da26ddb5e6040a94a39cf410021ed5cffd18836
willzhen <willzhen@tencent.com>
2021-07-29 15:50:26 +0800

    Add X-TC-Uin

--- media_asset_service/upload_part.go
+++ media_asset_service/upload_part.go
@@ -45,3 +45,39 @@ func UploadPart(header map[string]string, uri string, filebuf []byte) (*response
 	}
 	return rsp, nil
 }
+
+// PutObject 直接上传
+func PutObject(header map[string]string, uri string, filebuf []byte) (*response.PutObjectResponse, error) {
+	rsp := &response.PutObjectResponse{}
+	req, err := http.NewRequest(http.MethodPut, uri, bytes.NewBuffer(filebuf))
+	if err != nil {
+		errstr := fmt.Sprintf("[%s]UploadPart make NewRequest error: %s", uri, err.Error())
+		return nil, errors.New(errstr)
+	}
+	if header == nil {
+		req.Header.Set("Content-Type", "application/octet-stream")
+	} else {
+		for head, value := range header {
+			req.Header.Set(head, value)
+		}
+	}
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		errstr := fmt.Sprintf("[%s]UploadPart do request error: %s", uri, err.Error())
+		return nil, errors.New(errstr)
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		errstr := fmt.Sprintf("[%s]UploadPart status error, statuscode: %d, reason: %s",
+			uri, response.StatusCode, response.Status)
+		return nil, errors.New(errstr)
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	fmt.Print(string(data))
+	err = json.Unmarshal(data, rsp)
+	if err != nil {
+		errstr := fmt.Sprintf("[%s]UploadPart response protocol error %s", uri, err.Error())
+		return nil, errors.New(errstr)
+	}
+	return rsp, nil
+}
