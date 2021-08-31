936815d2a2780743de54b2d6d30d8625323a358a
willzhen <willzhen@tencent.com>
2021-08-31 16:17:24 +0800

    Add create medias api

---
+++ create_medias.go
@@ -0,0 +1,60 @@
+package mediaassetsdk
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/Tencent-Ti/ti-sign-go/tisign"
+	"github.com/Tencent-media-asset-system-sdk/media-asset-go-sdk/common"
+	mediaassetservice "github.com/Tencent-media-asset-system-sdk/media-asset-go-sdk/media_asset_service"
+	"github.com/Tencent-media-asset-system-sdk/media-asset-go-sdk/media_asset_service/request"
+	"github.com/Tencent-media-asset-system-sdk/media-asset-go-sdk/media_asset_service/response"
+)
+
+// ModifyExpireTime 修改文件过期时间，当前时间算起来，有效时间为 days 天
+func (m *MediaAssetClient) CreateMedias(req *request.CreateMediasRequest) (
+	rsp *response.CreateMediasResponse, err error) {
+	if m.Port == 0 {
+		m.Port = 80
+	}
+	action := "CreateMedias"
+	service := "app-cdn4aowk"
+	version := "2021-02-26"
+	headerContent := tisign.HttpHeaderContent{
+		XTCAction:   action,             // 请求接口
+		XTCService:  service,            // 接口所属服务名
+		XTCVersion:  version,            // 接口版本
+		ContentType: "application/json", // http请求的content-type, 当前网关只支持: application/json  multipart/form-data
+		HttpMethod:  "POST",             // http请求方法，当前网关只支持: POST GET
+		Host:        m.Host,             // 访问网关的host
+	}
+	uri := ""
+	header := map[string]string{}
+	req.TIBusinessID = uint32(m.TIBusinessID)
+	req.TIProjectID = uint32(m.TIProjectID)
+	req.Action = action
+	if m.Inner {
+		req.RequestID = common.GenerateRandomString(32)
+		req.Uin = m.InnerUserName
+		req.SubAccountUin = m.InnerUserName
+		uri = m.InnerMediaAssetEndPoint + "/" + action
+		header = nil
+	} else {
+		uri = fmt.Sprintf("http://%s:%d/gateway", m.Host, m.Port)
+		ts := tisign.NewTiSign(headerContent, m.SecretID, m.SecretKey)
+		header, _ = ts.CreateSignatureInfo()
+	}
+	maxTry := 3
+	for i := 0; i < maxTry; i++ {
+		err = mediaassetservice.HttpPost(uri, header, req, rsp)
+		if rsp.Response.ApiError != nil {
+			bys, _ := json.Marshal(rsp)
+			err = errors.New("Response error: " + string(bys))
+		}
+		if err == nil {
+			break
+		}
+	}
+	return rsp, err
+}
---
+++ media_asset_service/request/create_medias_request.go
@@ -0,0 +1,17 @@
+package request
+
+// CreateMediasRequest
+type CreateMediasRequest struct {
+	CommonRequest
+	UploadMediaSet []UploadMedia `json:"UploadMediaSet" validate:"required,dive"`
+	Inner          bool          `json:"Inner"`
+}
+
+// UploadMedia
+type UploadMedia struct {
+	Name       string    `json:"Name" validate:"required"`
+	LocalPath  string    `json:"LocalPath"`
+	MediaURL   string    `json:"MediaURL"`
+	ContentMD5 string    `json:"ContentMD5"`
+	MediaMeta  MediaMeta `json:"MediaMeta"`
+}
