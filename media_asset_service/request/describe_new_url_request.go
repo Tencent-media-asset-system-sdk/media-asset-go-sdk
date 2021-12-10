a49158c64171d1d00f273677bacabe70b07d4763
willzhen <willzhen@tencent.com>
2021-12-10 17:36:50 +0800

    Fixed DescribeNewURLRequest

--- media_asset_service/request/describe_new_url_request.go
+++ media_asset_service/request/describe_new_url_request.go
@@ -3,6 +3,6 @@ package request
 // DescribeNewURLRequest
 type DescribeNewURLRequest struct {
 	CommonRequest
-	URLSet []string `json:"url_set" validate:"required,dive"`
+	URLSet []string `json:"URLSet" validate:"required,dive"`
 	Inner  bool     `json:"Inner"`
 }
